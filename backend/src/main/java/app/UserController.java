94c1eb1a9a6cfe98befd172effa2cdb52449a537
moralss <jeramoral@gmail.com>
2019-06-20 12:06:35 +0200

    added my code into my folder

---
+++ backend/src/main/java/app/UserController.java
@@ -0,0 +1,36 @@
+package app;
+
+import app.domain.Approval;
+import app.domain.Article;
+import app.service.ArticleWorkflowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private ArticleWorkflowService service;
+
+    @PostMapping("/submit")
+    public void submit(@RequestBody Article article) {
+        service.startProcess(article);
+    }
+
+
+    @CrossOrigin
+    @GetMapping("/tasks")
+    public List<Article> getTasks(@RequestParam String assignee) {
+        return service.getTasks(assignee);
+    }
+
+    @CrossOrigin
+    @PostMapping("/review")
+    public void review(@RequestBody Approval approval) {
+        service.submitReview(approval);
+    }
+}
+
